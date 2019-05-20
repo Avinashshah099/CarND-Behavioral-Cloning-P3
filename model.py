bd1a402dad5c50a0cffcaf2e42b70c9aada1b257
Marcin Sielski <marcin.sielski@gmail.com>
2019-05-20 22:05:19 +0000

    Writeup report.

--- model.py
+++ model.py
@@ -2,94 +2,149 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D, Dropout
+from keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Cropping2D
 from keras.callbacks import ModelCheckpoint
+from keras.utils import plot_model
 from sklearn.model_selection import train_test_split
 import sklearn
 from math import ceil
 from random import shuffle
-import matplotlib.pyplot as plt
 from normalization import Normalization
 
-samples = []
-for i in range(0,3):
-    with open('./data/track'+str(i)+'/driving_log.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for sample in reader:
-            samples.append(sample)
+class Model:
 
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-def generator(samples, batch_size=32):
-    num_samples = len(samples)
-    correction=0.2
-    while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
+    """
+    
+        Model class
+    
+    """
 
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                image = cv2.imread(batch_sample[0])
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                angle = float(batch_sample[3])
-                images.append(image)
-                angles.append(angle)
-                images.append(cv2.flip(image, 1))
-                angles.append(-angle)
+    def __init__(self):
+        
 
-                image = cv2.imread(batch_sample[1])
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                angle = float(batch_sample[3]) + correction
-                images.append(image)
-                angles.append(angle)
-                images.append(cv2.flip(image, 1))
-                angles.append(-angle)
+        """
+        
+            Model constructor
+            
+        """
+        
+        samples = []
+        for i in range(0,3):
+            with open('./data/track'+str(i)+'/driving_log.csv') as csvfile:
+                reader = csv.reader(csvfile)
+                for sample in reader:
+                    samples.append(sample)
+        
+        # Split images to test set and validation set.
+        self.train_samples, self.validation_samples = train_test_split(samples, test_size=0.2)
+        train = len(self.train_samples)
+        validation = len(self.validation_samples)
+        total = train + validation
+        print('Train set size: {} ({}%)'.format(train, round(train*100/total)))
+        print('Validation set size: {} ({}%)'.format(validation, round(validation*100/total)))
+        print('Total size: {} (100%)'.format(total))
+        print('Total train images number: {} ({}%)'.format(6*train, round(train*100/total)))
+        print('Total validation images nimber: {} ({}%)'.format(6*validation, round(validation*100/total)))
+        print('Total images number: {} (100%)'.format(6*total))
 
-                image = cv2.imread(batch_sample[2])
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                angle = float(batch_sample[3]) - correction
-                images.append(image)
-                angles.append(angle)
-                images.append(cv2.flip(image, 1))
-                angles.append(-angle)
 
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
 
-# Set our batch size
-batch_size=16
+    def generator(self, samples, batch_size=16):
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=batch_size)
-validation_generator = generator(validation_samples, batch_size=batch_size)
 
-#ch, row, col = 3, 80, 320  # Trimmed image format
+        """
+            Images batch generator that delivers batch_size mutiplied by six images. Each batch consist of images from center, left and right camera and its
+            horizontally flipped representation.
+            
+            samples    - array with paths to images and steering angles,
+            batch_size - size of batch of images to be returned.
+            
+            
+        """
+        
+        num_samples = len(samples)
+        correction=0.2
+        while 1: # Loop forever so the generator never terminates
+            shuffle(samples)
+            for offset in range(0, num_samples, batch_size):
+                batch_samples = samples[offset:offset+batch_size]
 
-model_checkpoint = ModelCheckpoint('model.h5', verbose=1, save_best_only=True)
+                images = []
+                angles = []
+                for batch_sample in batch_samples:
+                    image = cv2.imread(batch_sample[0])
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    angle = float(batch_sample[3])
+                    images.append(image)
+                    angles.append(angle)
+                    images.append(cv2.flip(image, 1))
+                    angles.append(-angle)
 
-model = Sequential()
-model.add(Cropping2D(cropping=((55,25), (0, 0)), input_shape=(160, 320, 3)))
-#model.add(Lambda(lambda x: normalization, input_shape=(80, 320, 3)))
-model.add(Normalization())
+                    image = cv2.imread(batch_sample[1])
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    angle = float(batch_sample[3]) + correction
+                    images.append(image)
+                    angles.append(angle)
+                    images.append(cv2.flip(image, 1))
+                    angles.append(-angle)
 
-model.add(Conv2D(24, (5, 5), strides=(2,2), activation='relu'))
-model.add(Conv2D(36, (5, 5), strides=(2,2), activation='relu'))
-model.add(Conv2D(48, (5, 5), strides=(2,2), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Flatten())
-model.add(Dense(100))
-#model.add(Droput(0.5))
-model.add(Dense(50))
-#model.add(Droput(0.5))
-model.add(Dense(10))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit_generator(train_generator, steps_per_epoch=ceil(len(train_samples)/batch_size),
-validation_data=validation_generator, validation_steps=ceil(len(validation_samples)/batch_size),
-epochs=5, verbose=1, callbacks=[model_checkpoint])
+                    image = cv2.imread(batch_sample[2])
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    angle = float(batch_sample[3]) - correction
+                    images.append(image)
+                    angles.append(angle)
+                    images.append(cv2.flip(image, 1))
+                    angles.append(-angle)
 
+                X_train = np.array(images)
+                y_train = np.array(angles)
+                yield sklearn.utils.shuffle(X_train, y_train)
+
+
+    def train(self, filename='model.h5', batch_size=16):
+        
+
+        """
+        
+            Train the model and store results in specified file.
+            
+            filename - name of the file. 
+            
+        """
+        
+        # Create model
+        model = Sequential()
+        
+        # Preprocess the images
+        model.add(Cropping2D(cropping=((55,25), (0, 0)), input_shape=(160, 320, 3))) # trimming
+        model.add(Normalization())                                                   # normalization
+
+        # Create network architecture based on https://devblogs.nvidia.com/deep-learning-self-driving-cars/
+        model.add(Conv2D(24, (5, 5), strides=(2,2), activation='relu'))
+        model.add(Conv2D(36, (5, 5), strides=(2,2), activation='relu'))
+        model.add(Conv2D(48, (5, 5), strides=(2,2), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(100))
+        model.add(Dense(50))
+        model.add(Dense(10))
+        model.add(Dense(1))
+        model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        plot_model(model, to_file='images/model.png', show_shapes=True)
+        
+        # train the model
+        history = model.fit_generator(self.generator(self.train_samples, batch_size=batch_size), steps_per_epoch=ceil(len(self.train_samples)/batch_size),
+        validation_data=self.generator(self.validation_samples, batch_size=batch_size), validation_steps=ceil(len(self.validation_samples)/batch_size),
+        epochs=5, verbose=1, callbacks=[ModelCheckpoint(filename, verbose=1, save_best_only=True)])
+        print(history.history.keys())
+        print('Loss:')
+        print(history.history['loss'])
+        print('Validation Loss:')
+        print(history.history['val_loss'])
+        print('Accuracy:')
+        print(history.history['acc'])
+        
+
+Model().train()
---
+++ writeup_report.md
@@ -0,0 +1,123 @@
+# **Behavioral Cloning** 
+
+## Writeup
+
+**Behavioral Cloning Project**
+
+The goals / steps of this project are the following:
+* Use the simulator to collect data of good driving behavior
+* Build, a convolution neural network in Keras that predicts steering angles from images
+* Train and validate the model with a training and validation set
+* Test that the model successfully drives around track one without leaving the road
+* Summarize the results with a written report
+
+
+[//]: # (Image References)
+
+[image1]: ./images/model.png "Model Visualization"
+[image2]: ./images/left_2019_05_12_22_34_15_157.jpg "Track1 Left Camera"
+[image3]: ./images/center_2019_05_12_22_34_15_157.jpg "Track1 Center Camera"
+[image4]: ./images/right_2019_05_12_22_34_15_157.jpg "Track1 Right Camera"
+[image5]: ./images/left_2019_05_19_18_08_24_634.jpg "Track2 Left Camera"
+[image6]: ./images/center_2019_05_19_18_08_24_634.jpg "Track2 Center Camera"
+[image7]: ./images/right_2019_05_19_18_08_24_634.jpg "Track2 Right Camera"
+[image8]: ./images/center_2019_05_12_22_34_15_157_FlipH.jpg "Track1 Horizontal Flip"
+[image9]: ./images/center_2019_05_19_18_08_24_634_FlipH.jpg "Track2 Horizontal Flip"
+
+## Rubric Points
+### Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
+
+---
+### Files Submitted & Code Quality
+
+#### 1. Submission includes all required files and can be used to run the simulator in autonomous mode
+
+My project includes the following files:
+* [model.py](./model.py) containing the script to create and train the model
+* [drive.py](./drive.py) for driving the car in autonomous mode
+* [normalization.py](./normalization.py) for defining normalization cutom layer
+* [model.h5](./mode.h5) containing a trained convolution neural network 
+* [writeup_report.md](./writeup_report.md) summarizing the results
+
+#### 2. Submission includes functional code
+Using the Udacity provided simulator and my drive.py file, the car can be driven autonomously around the track by executing:
+```bash
+python3 drive.py model.h5
+```
+
+#### 3. Submission code is usable and readable
+
+The [model.py](./model.py) file contains the code for training and saving the convolution neural network. The file shows the pipeline I used for training and validating the model, and it contains comments to explain how the code works.
+
+### Model Architecture and Training Strategy
+
+#### 1. An appropriate model architecture has been employed
+
+My model consists of a convolution neural network with 5x5 and 3x3 filter sizes and depths between 24 and 64 ([model.py](./model.py) lines 124-128) 
+
+The model includes RELU layers to introduce nonlinearity (code line 124-128), and the data is normalized in the model using custom Keras layer (code line 121). 
+
+Custom Normalization layer was introduced as a replacement for Keras Lambda layer in order to enable save and read model.h5 on diffrent platforms.
+
+#### 2. Attempts to reduce overfitting in the model
+
+Large data sets enabled to use model without dropout layers. 
+
+The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 138-140). The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
+
+#### 3. Model parameter tuning
+
+The model used an adam optimizer, so the learning rate was not tuned manually ([model.py](./model.py) line 134).
+
+#### 4. Appropriate training data
+
+Training data was chosen to keep the vehicle driving on the road. It was sufficiemt to use center lane driving data. 
+
+For details about how I created the training data, see the next section. 
+
+### Model Architecture and Training Strategy
+
+#### 1. Solution Design Approach
+
+The overall strategy for deriving a model architecture was to use convolution neural network. 
+
+My first step was to use a convolution neural network model similar to the LeNet-5. I thought this model might be appropriate because it is relatively simple and yet powerful.
+
+In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting. 
+
+To combat the overfitting, I replaced the model with more poweful architecture from [nVidia](https://devblogs.nvidia.com/deep-learning-self-driving-cars/).
+
+Then I tunned the model architecture to fit project requirements.
+
+The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track1. Suprisigly it did not fell off the track2 which I considered to be much more difficult to learn. To improve the driving behavior on the track1 I have doubled the track1 training data set to meet the size of the track2 training data set (track2 is twice as long as track1). Unfortunatelly it did not help. At the end I realized that although track2 is more difficult learn than track1, track2 has simplier color scheme (it consists mostly from green and gray colors). I figured out that I forgot to convert images from BGR to RGB before training.
+
+After implementing simple fix, the vehicle is able to drive autonomously around both tracks without leaving the road.
+
+#### 2. Final Model Architecture
+
+The final model architecture ([model.py](./model.py) lines 124-133) consisted of a convolution neural network with the following layers and layer sizes:
+
+![alt text][image1]
+
+#### 3. Creation of the Training Set & Training Process
+
+To capture good driving behavior, I recorded 12 laps on track1 (6 laps in each direction) and 6 laps on track2 (3 laps in each direction) using center lane driving. Here is an example images of center lane driving on both tracks:
+
+| Left Camera         | Center Camera       | Right Camera        |
+|:-------------------:|:-------------------:|:-------------------:|
+| ![alt text][image2] | ![alt text][image3] | ![alt text][image4] |
+| ![alt text][image5] | ![alt text][image6] | ![alt text][image7] |
+
+To augment the data set, I also flipped images and angles thinking that this would enforce model to better generalize features. For example, here are images that has then been flipped:
+
+| Normal              | Horizontally Flipped |
+|:-------------------:|:--------------------:|
+| ![alt text][image3] | ![alt text][image8]  |
+| ![alt text][image6] | ![alt text][image7]  |
+
+
+After the collection process, I had 170304 number of data points. I then preprocessed this data by cropping the images by (55,25) pixes from (top,bottom) and normalizing with `x/127.5 - 1.0` formula.
+
+I finally randomly shuffled the data set and put 20% of the data into a validation set. 
+
+I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was 3-5 as evidenced in [model.log](./model.log) file. I used an adam optimizer so that manually training the learning rate wasn't necessary.
